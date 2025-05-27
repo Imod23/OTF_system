a0c2e7e679c149c994819db5a77bd1afeb0085e0
Imod23 <148761780+Imod23@users.noreply.github.com>
2025-05-27 14:27:24 +0530

    Add files via upload

---
+++ IFineServices.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using OTF_System.Models;
+
+namespace OTF_System.Services
+{
+    public interface IFineService
+    {
+        Task<List<Fine>> GetAllFinesAsync();
+        Task<Fine> GetFineByIdAsync(string id);
+        Task AddFineAsync(Fine fine);
+        Task UpdateFineAsync(Fine fine);
+        Task<bool> DeleteFineAsync(string id);
+    }
+}
